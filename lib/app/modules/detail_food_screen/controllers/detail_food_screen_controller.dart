f2497ec5be6634d150c2e0a7c6a0e62874b0e247
Dadah Taufik <dadahtaufik@refactory.id>
2023-03-05 20:46:52 +0700

    feat/fetching get gonnect

---
+++ lib/app/modules/detail_food_screen/controllers/detail_food_screen_controller.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class DetailFoodScreenController extends GetxController {
+
+  final Rx<Color> iconColor = Colors.black26.obs;
+  void onClickButton() {
+    if (iconColor.value == Colors.black26) {
+      iconColor.value = Colors.redAccent;
+    } else {
+      iconColor.value = Colors.black26;
+    }
+  }
+
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {}
+
+}
